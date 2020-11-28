5cdbc87891956fbd51cc50e44a0f0501074c3e94
shabrinatalita <shabrinatalita50@gmail.com>
2020-11-28 16:06:10 +0700

    UI Database

---
+++ baju.php
@@ -0,0 +1,84 @@
+<?php
+error_reporting(0);
+    include 'db.php';
+    $mimin = mysqli_query($conn, "SELECT admin_telpon, admin_email, admin_address FROM tb_admin WHERE admin_id = 1");
+    $a = mysqli_fetch_object($mimin);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Baju</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+</head>
+<body>
+   <!-- Header -->
+    <header>
+        <div class="container">
+        <h1><a href="index.php">Toko Baju</a></h1>
+        <ul>
+            <li><a href="baju.php">Produk</a></li>
+        </ul>
+        </div>
+    </header>
+
+    <!--Pencarian-->
+    <div class="cari">
+        <div class="container">
+            <form action="baju.php">
+                <input type="text" name="cari" placeholder="Cari Baju" value="<?php echo $_GET['cari'] ?>">
+                <input type="hidden" name="kat" value=<?php echo $_GET['kat'] ?>>
+                <input type="submit" name="search" value="Cari Baju">
+            </form>
+        </div>
+    </div>
+
+
+    <!--Produk Baru-->
+    <div class="section">
+        <div class="container">
+            <h3>Baju</h3>
+            <div class="box">
+                <?php 
+                    if($_GET['cari'] != '' || $_GET['kat'] != '') {
+                        $where = "AND product_name LIKE '%".$_GET['cari']."%' AND category_id LIKE '%".$_GET['kat']."%' ";
+                    }
+
+                    $baju = mysqli_query($conn, "SELECT * FROM tb_product WHERE product_status = 1 $where ORDER BY product_id DESC");
+                    if(mysqli_num_rows($baju) > 0){
+                        while($p = mysqli_fetch_array($baju)){
+                ?>
+                    <a href="detail-baju.php?id=<?php echo $p['product_id'] ?>">
+                        <div class="col-4">
+                            <img src="baju/<?php echo $p['product_image'] ?>">
+                            <p class="nama"><?php echo substr($p['product_name'], 0, 30) ?></p>   
+                            <p class="harga">Rp. <?php echo number_format($p['product_price'])?></p>
+                        </div>
+                    </a>
+                <?php }}else { ?>
+                    <p>Baju Tidak Tersedia</p>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+
+    <!--footer-->
+    <div class="footer">
+        <div class="container">
+            <h4>Alamat</h4>
+            <p><?php echo $a->admin_address ?></p>
+
+            <h4>Email</h4>
+            <p><?php echo $a->admin_email ?></p>
+
+            <h4>Telepon</h4>
+            <p><?php echo $a->admin_telpon ?></p>
+
+            <small>Copyright &copy; 2020 - Toko Baju.</small>
+        </div>
+    </div>
+</body>
+</html>
---
+++ clothes.php
@@ -0,0 +1,88 @@
+<?php
+    session_start();
+    include 'db.php';
+    if($_SESSION['status_login'] != true) {
+        echo '<script>window.location="login.php"</script>';
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Baju</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+</head>
+<body>
+   <!-- Header -->
+    <header>
+        <div class="container">
+        <h1><a href="dashboard.php">Toko Baju</a></h1>
+        <ul>
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="category.php">Kategori</a></li>
+            <li><a href="clothes.php">Data Baju</a></li>
+            <li><a href="logout.php">Log Out</a></li>
+        </ul>
+        </div>
+    </header>
+
+    <!-- Content -->
+    <div class="section">
+        <div class="container">
+            <h3>Baju</h3>
+            <div class="box">
+                <p><a href="tambah-baju.php">Tambah Baju</a></p>
+                <table border="1" cellspacing="0" class="tabel">
+                    <thead>
+                        <tr>
+                            <th width="60px">No</th>
+                            <th>Kategori</th>
+                            <th>Judul Baju</th>
+                            <th>Harga</th>
+                            <th>Gambar</th>
+                            <th>Status</th>
+                            <th width="150px">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                            $num = 1;
+                            $baju = mysqli_query($conn, "SELECT * FROM tb_product LEFT JOIN tb_category USING (category_id) ORDER BY product_id DESC");
+                            if(mysqli_num_rows($baju) > 0) {
+                            while($row = mysqli_fetch_array($baju)){
+                        ?>
+                        <tr>
+                            <td><?php echo $num++ ?></td>
+                            <td><?php echo $row['category_name'] ?></td>
+                            <td><?php echo $row['product_name'] ?></td>
+                            <td>Rp-<?php echo number_format($row['product_price']) ?></td>
+                            <td><a href="baju/<?php echo $row['product_image'] ?>" target="_blank"><img src="baju/<?php echo $row['product_image'] ?>" width="100px"></a></td>
+                            <td><?php echo ($row['product_status'] == 0)? 'Habis':'Stock Tersedia'; ?></td>
+                            <td>
+                                <a href="edit-baju.php?id=<?php echo $row['product_id'] ?>">Edit</a> || <a href="delete-category.php?idp=<?php echo $row['product_id'] ?>" onclick="return confirm('Apakah Anda Yakin Ingin Menghapus?')">Hapus</a>
+                            </td>
+                        </tr>
+                        <?php } }else { ?>
+                            <tr>
+                                <td colspan="7">Tidak ada data</td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <small>Copyright &copy; 2020 - Toko Baju.</small>
+        </div>
+    </footer>
+</body>
+</html>
